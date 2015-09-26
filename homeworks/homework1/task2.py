e21e6f1824e189b824b76b5ed3379d4068a9ad48
kirillv5 <kirillv5@yandex.ru>
2015-09-26 15:01:53 +0300

    Update task2.py

--- homeworks/homework1/task2.py
+++ homeworks/homework1/task2.py
@@ -1,8 +1,8 @@
-l=[int(i) for i in input().split()]
-s=0
-for i in range (0, len(l)):
-    s+=int(l[i])
-print(s/len(l))
+l = [int(i) for i in input().split()]
+s = 0
+for i in range(0, len(l)):
+    s += int(l[i])
+print(s / len(l))
 
 
 
