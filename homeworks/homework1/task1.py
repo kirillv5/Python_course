cdf72ec54f1c1c290006908b6e6ed56f660e04aa
kirillv5 <kirillv5@yandex.ru>
2015-09-26 14:59:26 +0300

    Update task1.py

--- homeworks/homework1/task1.py
+++ homeworks/homework1/task1.py
@@ -1,24 +1,22 @@
-word=str(input())
-number=str(input())
-n=(int(number))
-c=int(number[len(number)-1])
-d=int(number[len(number)-2])
-if word=='утюг':
-    if n==1 or c==1 and d!=1:
+word = str(input())
+number = str(input())
+n = (int(number))
+c = int(number[len(number) - 1])
+d = int(number[len(number) - 2])
+if word == 'утюг':
+    if n == 1 or c == 1 and d != 1:
         print(n, "утюг")
-    elif 2<=n<=4 or 2<=c<=4 and d!=1:
+    elif 2 <= n <= 4 or 2 <= c <= 4 and d != 1:
         print(n, "утюга")
     else:
         print(n, "утюгов")
-if word=='ложка':
-    if n==1 or c==1 and d!=1:
+if word == 'ложка':
+    if n == 1 or c == 1 and d != 1:
         print(n, "ложка")
-    elif 2<=n<=4 or 2<=c<=4 and d!=1:
+    elif 2 <= n <= 4 or 2 <= c <= 4 and d != 1:
         print(n, "ложки")
     else:
         print(n, "ложек")
-
-
 
 
 
