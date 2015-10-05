1b3d4bddb2aadd9f3f4fb000b9cb1f0457f92649
kirillv5 <kirillv5@yandex.ru>
2015-10-05 16:28:33 +0300

    Update task1.py

--- homeworks/homework2/task1.py
+++ homeworks/homework2/task1.py
@@ -7,11 +7,11 @@ def combination(n, k):
         lst = [i for i in range(1, x + 1)]
         for i in lst:
             c *= i
-        return(c)
+        return c
     if k > n:
-        return(0)
+        return 0
     elif n == k or k == 0:
-        return(1)
+        return 1
     else:
         return(factorial(n) / (factorial(k) * factorial(n - k)))
 print(int(combination(n, k))) 
