c2d21bfce5f9d890a63b84965fbf0e93b58ab165
kirillv5 <kirillv5@yandex.ru>
2015-10-05 16:23:12 +0300

    Update task4.py

--- homeworks/homework2/task4.py
+++ homeworks/homework2/task4.py
@@ -1,17 +1,16 @@
-lst_output = []
-
-
 def rpfilter(a, *arg):
+    lst_output = []
     def euclid(n, m):
         if n % m == 0:
-            return(m)
+            return m
         else:
-            return(euclid(m, n % m))
+            return euclid(m, n % m)
     for i in arg:
         if euclid(a, i) == 1:
             lst_output.append(i)
+    return lst_output
 lst_input = (int(i) for i in input().split())  
-rpfilter(*lst_input)
+lst_output = rpfilter(*lst_input)
 if lst_output != []:
     for i in lst_output:
         print(i, end=' ')
