bebd3a3e79c69be532fba994e87a0b0cb8dbbd35
kirillv5 <kirillv5@yandex.ru>
2015-10-05 16:26:18 +0300

    Update task3.py

--- homeworks/homework2/task3.py
+++ homeworks/homework2/task3.py
@@ -1,7 +1,7 @@
 def euclid(n, m):
     if n % m == 0:
-        return(m)
+        return m
     else:
-        return(euclid(m, n % m))
+        return euclid(m, n % m)
 n, m = (int(i) for i in input().split())        
 print(euclid(n, m))
