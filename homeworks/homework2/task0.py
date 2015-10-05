e6f15fc467d980a2221389898e6f1b74b4a26e0d
kirillv5 <kirillv5@yandex.ru>
2015-10-05 15:28:50 +0300

    Update task0.py

--- homeworks/homework2/task0.py
+++ homeworks/homework2/task0.py
@@ -1,21 +1,23 @@
 word = str(input())
 words = []
 number = str(input())
-n=(int(number))
-c=int(number[len(number)-1])
-d=int(number[len(number)-2])
-if word == 'утюг':
-    words = ['утюг', 'утюга', 'утюгов']
-elif word == 'ложка' :
-    words = ['ложка', 'ложки', 'ложек']
-elif word == 'гармошка' :
-     words = ['гармошка', 'гармошки', 'гармошек']
-elif word == 'чайник' :
-    words = ['чайник', 'чайника', 'чайников']
+n = (int(number))
+c = int(number[len(number) - 1])
+d = int(number[len(number) - 2])
+if word == 'СѓС‚СЋРі':
+    words = ['СѓС‚СЋРі', 'СѓС‚СЋРіР°', 'СѓС‚СЋРіРѕРІ']
+elif word == 'Р»РѕР¶РєР°':
+    words = ['Р»РѕР¶РєР°', 'Р»РѕР¶РєРё', 'Р»РѕР¶РµРє']
+elif word == 'РіР°СЂРјРѕС€РєР°':
+    words = ['РіР°СЂРјРѕС€РєР°', 'РіР°СЂРјРѕС€РєРё', 'РіР°СЂРјРѕС€РµРє']
+elif word == 'С‡Р°Р№РЅРёРє':
+    words = ['С‡Р°Р№РЅРёРє', 'С‡Р°Р№РЅРёРєР°', 'С‡Р°Р№РЅРёРєРѕРІ']
+    
+    
 def plural(n, words):
-    if n==1 or c==1 and d!=1:
+    if n == 1 or c == 1 and d != 1:
         print(n, words[0])
-    elif 2<=n<=4 or 2<=c<=4 and d!=1:
+    elif 2 <= n <= 4 or 2 <= c <= 4 and d != 1:
         print(n, words[1])
     else:
         print(n, words[2])
