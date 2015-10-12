22a1b3e6ccc62bafa14997d1cab6d44f2ecd7348
kirillv5 <kirillv5@yandex.ru>
2015-10-12 18:57:25 +0300

    Update task1.py

--- homeworks/homework2/task1.py
+++ homeworks/homework2/task1.py
@@ -2,16 +2,10 @@ n, k = (int(i) for i in input().split())
 
 
 def combination(n, k):
-    def factorial(x):
-        c = 1
-        lst = [i for i in range(1, x + 1)]
-        for i in lst:
-            c *= i
-        return c
     if k > n:
         return 0
     elif n == k or k == 0:
         return 1
     else:
-        return factorial(n) / (factorial(k) * factorial(n - k))
-print(int(combination(n, k))) 
+        return combination(n - 1, k - 1) + combination(n - 1, k)
+print(combination(n, k))
