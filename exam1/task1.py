901eab5c3919b0600754e4cbd1501a97ecae0da9
kirillv5 <kirillv5@yandex.ru>
2015-11-07 17:49:30 +0300

    task

---
+++ exam1/task1.py
@@ -0,0 +1,16 @@
+inf = open("yazkora.txt", "r")
+text = inf.read()
+inf.close()
+sent = [i for i in text.replace("\n", " ").split(".")]
+for i in range(len(sent)):
+    sent[i] = sent[i].split()
+i = 0
+of = open("answer.txt", "w")
+while i != len(sent):
+    for j in sent[i]:
+        if "yo" in j[len(j) - 2:]:
+            of.write(j + " ")
+        else:
+            of.write("\n")
+    i+=1
+of.close()
